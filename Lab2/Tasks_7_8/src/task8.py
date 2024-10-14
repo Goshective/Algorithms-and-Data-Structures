fcc88f2a5a10240b74083c85c9cda6dc7b03b737
Goshective <den,skvortsoff@gmail.com>
2024-10-14 21:02:43 +0300

    Add reports and README files

--- Lab2/Tasks_7_8/src/task8.py
+++ Lab2/Tasks_7_8/src/task8.py
@@ -1,16 +1,4 @@
-def multiply_polinoms(n, A, B, a_i, b_i):
-    R = [0] * (2*n - 2)
-    if n == 1:
-        R[0] = A[a_i] * B[b_i]
-        return R
-    R[:n-2 + 1] = multiply_polinoms(n//2, A, B, a_i, b_i)
-    R[n: 2*n-2] = multiply_polinoms(n//2, A, B, a_i + n//2, b_i + n//2)
-
-    D0E1 = multiply_polinoms(n//2, A, B, a_i, b_i + n//2)
-    D1E0 = multiply_polinoms(n//2, A, B, a_i + n//2, b_i)
-
-    R[n//2:n+n//2 - 2 + 1] += D1E0 + D0E1
-    return R
+import os
 
 def sum_polynoms(A, B, shift_A=0, shift_B=0):
     len_r = max(len(A) + shift_A, len(B) + shift_B)
@@ -46,7 +34,8 @@ def multiply_polynoms(n, A, B):
     return R
 
 def main():
-    with open('input.txt8', 'r') as inp, open('output.txt', 'w') as out:
+    path = os.path.dirname(os.path.abspath(__file__)) + '/'
+    with open(path+'input8.txt', 'r') as inp, open(path+'output.txt', 'w') as out:
         n = int(inp.readline())
         a = [int(x) for x in inp.readline().split()]
         b = [int(x) for x in inp.readline().split()]
