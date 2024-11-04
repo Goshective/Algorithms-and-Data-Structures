92a8bb5fdbb36ab1c2e8272ec89591695f57ede4
Goshective <den,skvortsoff@gmail.com>
2024-11-05 01:14:42 +0300

    Return previous module for working project

--- test_utils.py
+++ test_utils.py
@@ -29,3 +29,22 @@ class ConsoleTimeMemory:
         func(*args)
         memory = tracemalloc.get_traced_memory()[1]
         return memory
+
+
+def output_design(test_name, func, *args): # to deletion after changing system
+    tracemalloc.start()
+    t_start = time.perf_counter()
+    func(*args)
+    res_time = time.perf_counter() - t_start
+    res_memory = tracemalloc.get_traced_memory()[1]
+
+    if res_memory < 1024:
+        out_mem = f'{res_memory} Байт'
+    elif res_memory < 1024**2:
+        out_mem = f'{round(res_memory / 1024, 1)} Килобайт'
+    else:
+        out_mem = f'{round(res_memory / (1024**2), 1)} Мегабайт'
+
+    print(f"Тест {test_name}:")
+    print("Время работы: %s секунд " % (res_time), end='\n')
+    print("Затрачено памяти:", out_mem)
