a1341db5d0e49d73a2acb5f6f33d6a25bcae5647
Goshective <den,skvortsoff@gmail.com>
2024-11-06 10:42:34 +0300

    Fix memory counting in tests

--- test_utils.py
+++ test_utils.py
@@ -28,23 +28,5 @@ class ConsoleTimeMemory:
         tracemalloc.start()
         func(*args)
         memory = tracemalloc.get_traced_memory()[1]
+        tracemalloc.stop()
         return memory
-
-
-def output_design(test_name, func, *args): # to deletion after changing system
-    tracemalloc.start()
-    t_start = time.perf_counter()
-    func(*args)
-    res_time = time.perf_counter() - t_start
-    res_memory = tracemalloc.get_traced_memory()[1]
-
-    if res_memory < 1024:
-        out_mem = f'{res_memory} Байт'
-    elif res_memory < 1024**2:
-        out_mem = f'{round(res_memory / 1024, 1)} Килобайт'
-    else:
-        out_mem = f'{round(res_memory / (1024**2), 1)} Мегабайт'
-
-    print(f"Тест {test_name}:")
-    print("Время работы: %s секунд " % (res_time), end='\n')
-    print("Затрачено памяти:", out_mem)
