68b19c055ecc038fa76d0f9fcd362e831a8fd67d
Goshective <den,skvortsoff@gmail.com>
2024-11-21 20:45:50 +0300

    Add task2 tests

---
+++ Lab5/Task_plus_2/tests/test.py
@@ -0,0 +1,76 @@
+import unittest
+import sys
+import os
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(PATH, '..', '..', '..'))
+
+from Lab5.Task_plus_2.src.main import solution
+from test_utils import (ConsoleTimeMemory as TM, MB)
+
+
+class TestsTreeHeight(unittest.TestCase):
+    def test_should_find_simple_height(self):
+        # given
+        inp = [4, -1, 4, 1, 1]
+        excepted_res = 3
+
+        # when
+        res = solution(inp)
+
+        # then
+        self.assertEqual(res, excepted_res)
+
+    def test_should_find_height_of_straight_tree(self):
+        # given
+        inp = [-1, 0, 4, 0, 3]
+        excepted_res = 4
+
+        # when
+        res = solution(inp)
+
+        # then
+        self.assertEqual(res, excepted_res)
+
+    def test_should_find_height_of_one_node(self):
+        # given
+        inp = [-1]
+        excepted_res = 1
+
+        # when
+        res = solution(inp)
+
+        # then
+        self.assertEqual(res, excepted_res)
+
+    def check_time_memory_limit(self, res_time, res_memory):
+        # given
+
+        expected_memory = 512 * MB
+        expected_time = 3
+
+        # when
+        # then
+        
+        self.assertLessEqual(res_time, expected_time)
+        self.assertLessEqual(res_memory, expected_memory)
+    
+    def test_should_fit_time_memory_limit(self):
+        # given
+        test_data = [('100 элементов', [-1] + [i // 2 for i in range(100)]),
+                     ('10e3 элементов', [-1] + [i // 4 for i in range(1000)]),
+                     ('10e5 элементов', [-1] + [i // 8 for i in range(10**5)])]
+
+        for test_name, input_by_size in test_data:
+            # when
+            res_time = TM.count_time(solution, input_by_size)
+            res_memory = TM.count_memory(solution, input_by_size)
+
+            TM.output_design(test_name, res_time, res_memory)
+
+            # then
+            self.check_time_memory_limit(res_time, res_memory)
+
+
+if __name__ == "__main__":
+    unittest.main()
