bbafb901f9d9980d6e9e42e743dc2f0cd680645f
Goshective <den,skvortsoff@gmail.com>
2024-11-26 21:28:40 +0300

    Add task7 solution with tests

---
+++ Lab5/Task_main_7/src/main.py
@@ -0,0 +1,72 @@
+import os
+import sys
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(PATH, '..', '..', '..'))
+
+from Lab5.utils import read_n_lst, write_lst_file
+
+
+def left(i):
+    return 2 * (i+1) - 1
+
+def right(i):
+    return 2 * (i+1)
+
+def MinHeapify(lst, i, heap_size=None):
+    if heap_size is None:
+        heap_size = len(lst)
+
+    while True:
+        _l = left(i)
+        r = right(i)
+        if _l > r:
+            break
+        if _l < heap_size and lst[_l] < lst[i]:
+            smallest = _l
+        else:
+            smallest = i
+
+        if r < heap_size and lst[r] < lst[smallest]:
+            smallest = r
+
+        if smallest != i:
+            lst[i], lst[smallest] = lst[smallest], lst[i]
+            i = smallest
+        else:
+            break
+
+def BuildMinHeap(lst):
+    heap_size = len(lst)
+    swaps = []
+    for i in range(heap_size//2, -1, -1):
+        MinHeapify(lst, i)
+
+    return swaps
+
+def HeapSort(lst):
+    BuildMinHeap(lst)
+    heap_size = len(lst)
+    for i in range(len(lst)-1, 0, -1):
+        lst[0], lst[i] = lst[i], lst[0]
+        heap_size = heap_size - 1
+        MinHeapify(lst, 0, heap_size)
+
+
+def solution(lst, sort_func=HeapSort):
+    res = lst.copy()
+    if sort_func == HeapSort:
+        sort_func(res)
+    else:
+        sort_func(res, 0, len(res)-1)
+    return res
+
+
+def main():
+    lst = read_n_lst(os.path.join(PATH, 'txtf', 'input.txt'))[1]
+    res = solution(lst)
+    write_lst_file(os.path.join(PATH, 'txtf', 'output.txt'), res)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Lab5/Task_main_7/tests/test.py
@@ -0,0 +1,82 @@
+import unittest
+import sys
+import os
+from random import shuffle
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(PATH, '..', '..', '..'))
+
+from Lab2.Task_main_1.src.main import merge_sort
+from Lab3.Task_main_1.src.main import RandomizedQuickSort
+from Lab5.Task_main_7.src.main import solution, HeapSort
+
+from test_utils import (ConsoleTimeMemory as TM, MB)
+
+
+class TestHeapSort(unittest.TestCase):
+    def test_should_sort_example(self):
+        # given
+
+        inp = [31, 41, 59, 26, 41, 58]
+        expected_res = [26, 31, 41, 41, 58, 59][::-1]
+
+        # when
+
+        res = solution(inp)
+
+        # then
+
+        self.assertEqual(res, expected_res)
+
+    def test_should_sort_growing_sequence(self):
+        # given
+
+        inp = [1, 8, 4, 2, 3, 7, 5, 6, 9, 0]
+        expected_res = list(range(9, -1, -1))
+
+        # when
+
+        res = solution(inp)
+
+        # then
+
+        self.assertEqual(res, expected_res)
+
+    def check_time_memory_limit(self, res_time, res_memory):
+        # given
+
+        expected_memory = 256 * MB
+        expected_time = 2
+
+        # when
+        # then
+
+        self.assertLessEqual(res_time, expected_time)
+        self.assertLessEqual(res_memory, expected_memory)
+    
+    def test_should_fit_time_memory_limit(self):
+            for func_name, func in (('Merge sort:', merge_sort), 
+                                    ('Randomized Quick sort:', RandomizedQuickSort), 
+                                    ('Heap sort:', HeapSort)):
+                # given
+                test_data = []
+                for i in (10**3, 10**4, 10**5):
+                    inp_lst = [(10**4 * i)*(-1)**(i%2) for i in range(i)]
+                    shuffle(inp_lst)
+                    test_data.append((f'{i} элементов', (inp_lst, func)))
+
+                for test_id, (test_name, input_by_size) in enumerate(test_data):
+                    # when
+                    res_time = TM.count_time(solution, *input_by_size)
+                    res_memory = TM.count_memory(solution, *input_by_size)
+
+                    if test_id == 0:
+                        print('\n', func_name)
+                    TM.output_design(test_name, res_time, res_memory)
+
+                    # then
+                    self.check_time_memory_limit(res_time, res_memory)
+
+
+if __name__ == "__main__":
+    unittest.main()
